387e3e29a1b0e8ef50e52b401d6bc3c65983901a
DESKTOP-58H1IT8\Vy Tran <dangvy202@gmail.com>
2026-01-08 16:56:49 +0700

    FE structure

--- lumilingua-frontend/app/_layout.tsx
+++ lumilingua-frontend/app/_layout.tsx
@@ -1,6 +1,21 @@
 import { Stack } from "expo-router";
 import './global.css';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function RootLayout() {  
+  return (
+    <Stack>
+      <Stack.Screen 
+        name="(tabs)"
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Stack.Screen
+        name="course/[id]"
+        options={{
+          headerShown: false,
+        }}
+      />
+    </Stack>
+  )
 }
--- lumilingua-frontend/babel.config.js
+++ lumilingua-frontend/babel.config.js
@@ -1,8 +1,12 @@
 module.exports = function (api) {
-    api.cache(true);
-    return {
-        presets: [
-            ["babel-preset-expo", {jsxImportSource: "nativewind/label",}]
-        ],
-    }
-}
+  api.cache(true);
+  return {
+    presets: [
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
+      "nativewind/babel",
+    ],
+    plugins: [
+      "react-native-reanimated/plugin",
+    ],
+  };
+};
